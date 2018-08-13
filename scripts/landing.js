142e19ae984c55ccc099a66a2dfe5d879a6c6442
elveegee <lucasvangombos@gmail.com>
2018-08-13 14:29:49 -0400

    Checkpoint 23: Events: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
   var revealPoint = function(i) {
     for(var i=0; i < points.length; i++) {
@@ -8,11 +8,23 @@ var animatePoints = function() {
       points[i].style.transform = "scaleX(1) translateY(0)";
       points[i].style.msTransform = "scaleX(1) translateY(0)";
       points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-      }
+    }
   };
 
   revealPoint();
 
 };
 
-  animatePoints();
+window.onload = function() {
+    if (window.innerHeight > 950) {
+      animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+    });
+}
