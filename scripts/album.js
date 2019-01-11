c07517f340d55666d05b5e387eaca9142cc1f627
elveegee <lucasvangombos@gmail.com>
2019-01-10 19:52:04 -0500

    checkpoint 32: song name won't change in player bar when clicking through table

--- scripts/album.js
+++ scripts/album.js
@@ -1,48 +1,3 @@
-var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1885',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
-};
-
-var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
-
-var albumArchers = {
-      title: 'All the Nations Airports',
-      artist: 'Archers of Loaf',
-      label: 'Alias Records',
-      year: '1996',
-      albumArtUrl: 'assets/images/album_covers/archers.jpg',
-      songs: [
-          { title: 'Strangled by the Stereo Wire', duration: '3:26' },
-          { title: 'All the Nations Airports', duration: '3:43' },
-          { title: 'Scenic Pastures', duration: '5:55' },
-          { title: 'Worst Defense', duration: '8:21'},
-          { title: 'Attack of the Killer Bees', duration: '2:56'}
-      ]
-};
-
 var createSongRow = function(songNumber, songName, songLength) {
 
      var template =
@@ -58,19 +13,21 @@ var createSongRow = function(songNumber, songName, songLength) {
      var clickHandler = function() {
         var songNumber = $(this).attr('data-song-number');
 
-	      if (currentlyPlayingSong !== null) {
+	      if (currentlyPlayingSongNumber !== null) {
 		        // Revert to song number for currently playing song because user started playing new song.
-		          var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-		            currentlyPlayingCell.html(currentlyPlayingSong);
+		          var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+		            currentlyPlayingCell.html(currentlyPlayingSongNumber);
 	      }
-        if (currentlyPlayingSong !== songNumber) {
+        if (currentlyPlayingSongNumber !== songNumber) {
             // Switch from Play -> Pause button to indicate new song is playing.
             $(this).html(pauseButtonTemplate);
-            currentlyPlayingSong = songNumber;
-        } else if (currentlyPlayingSong === songNumber) {
+            currentlyPlayingSongNumber = songNumber;
+            currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+        } else if (currentlyPlayingSongNumber === songNumber) {
              // Switch from Pause -> Play button to pause currently playing song.
              $(this).html(playButtonTemplate);
-                 currentlyPlayingSong = null;
+             currentlyPlayingSongNumber = null;
+             currentSongFromAlbum = null;
         }
      };
 
@@ -78,7 +35,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
 
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(playButtonTemplate);
         }
      };
@@ -87,7 +44,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
 
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(songNumber);
         }
      };
@@ -95,7 +52,8 @@ var createSongRow = function(songNumber, songName, songLength) {
      $row.find('.song-item-number').click(clickHandler);
      $row.hover(onHover, offHover);
      return $row;
- };
+
+};
 
  var $albumTitle = $('.album-view-title');
  var $albumArtist = $('.album-view-artist');
@@ -104,6 +62,7 @@ var createSongRow = function(songNumber, songName, songLength) {
  var $albumSongList = $('.album-view-song-list');
 
  var setCurrentAlbum = function(album) {
+     currentAlbum = album;
      $albumTitle.text(album.title);
      $albumArtist.text(album.artist);
      $albumReleaseInfo.text(album.year + ' ' + album.label);
@@ -139,7 +98,9 @@ var createSongRow = function(songNumber, songName, songLength) {
 //Variables for button icons.
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
- var currentlyPlayingSong = null;
+ var currentAlbum = null;
+ var currentlyPlayingSongNumber = null;
+ var currentSongFromAlbum = null;
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1885',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+};
+
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumArchers = {
+      title: 'All the Nations Airports',
+      artist: 'Archers of Loaf',
+      label: 'Alias Records',
+      year: '1996',
+      albumArtUrl: 'assets/images/album_covers/archers.jpg',
+      songs: [
+          { title: 'Strangled by the Stereo Wire', duration: '3:26' },
+          { title: 'All the Nations Airports', duration: '3:43' },
+          { title: 'Scenic Pastures', duration: '5:55' },
+          { title: 'Worst Defense', duration: '8:21'},
+          { title: 'Attack of the Killer Bees', duration: '2:56'}
+      ]
+};
