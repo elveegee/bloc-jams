b06c3cdd53ded7a94750e7c52d18c37b532c0f9c
elveegee <lucasvangombos@gmail.com>
2019-01-11 20:42:05 -0500

    checkpoint 33 stuck

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,11 @@
 var setSong = function(songNumber) {
   currentlyPlayingSongNumber = parseInt(songNumber);
   currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+  currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+         // #2
+         formats: [ 'mp3' ],
+         preload: true
+     });
 };
 
 var getSongNumberCell = function (number){
@@ -83,15 +88,21 @@ var createSongRow = function(songNumber, songName, songLength) {
 	      }
         if (currentlyPlayingSongNumber !== songNumber) {
             // Switch from Play -> Pause button to indicate new song is playing.
-            $(this).html(pauseButtonTemplate);
             setSong(songNumber);
+            currentSoundFile.play();
+            $(this).html(pauseButtonTemplate);
+
             updatePlayerBarSong();
         } else if (currentlyPlayingSongNumber === songNumber) {
-             // Switch from Pause -> Play button to pause currently playing song.
-             $(this).html(playButtonTemplate);
-             $('.main-controls .play-pause').html(playerBarPlayButton);
-             currentlyPlayingSongNumber = null;
-             currentSongFromAlbum = null;
+            /* if (currentSoundFile.isPaused()) {
++                $(this).html(pauseButtonTemplate);
++                $('.main-controls .play-pause').html(playerBarPauseButton);
++                currentSoundFile.play();
++            } else {
++                $(this).html(playButtonTemplate);
++                $('.main-controls .play-pause').html(playerBarPlayButton);
++                currentSoundFile.pause()*/
++            }
         }
      };
 
@@ -181,6 +192,7 @@ var createSongRow = function(songNumber, songName, songLength) {
  var currentAlbum = null;
  var currentlyPlayingSongNumber = null;
  var currentSongFromAlbum = null;
+ var currentSoundFile = null;
 
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
